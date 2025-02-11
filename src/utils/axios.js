8fbb2ac15e644f247231a2ac3e4addb431c2a450
Mahmoud Elkomy <mahmoudelkomy996@gmail.com>
2025-02-11 18:36:47 +0200

    URL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://wblnjd.zeroonez.com/api/",
+  baseURL: "https://api.wabelnajd.com.sa/api/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
